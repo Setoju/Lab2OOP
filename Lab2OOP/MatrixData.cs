0a03cfa14df919d0354dc2cf8d36dbb3bd6c5af5
setoju48@gmail.com <setoju48@gmail.com>
2023-11-03 16:59:07 +0200

    Fixed some mistakes

--- Lab2OOP/MatrixData.cs
+++ Lab2OOP/MatrixData.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.Text.RegularExpressions;
+
 
 namespace Lab2OOP
 {    
@@ -11,46 +8,34 @@ namespace Lab2OOP
     {
         private double[,] _matrix;
         //Constructors                
-        public MyMatrix(MyMatrix other)
+        public MyMatrix(MyMatrix myMatrix) : this(myMatrix._matrix) 
         {
-            _matrix = (double[,])other._matrix.Clone();            
+
         }
         public MyMatrix(double[,] matrix)
-        {
-            _matrix = matrix;
+        {            
+            _matrix = (double[,])matrix.Clone();
         }
         public MyMatrix(double[][] matrix) 
         {
-            if (IsRectangular(matrix))
+            _matrix = new double[matrix.Length, matrix[0].Length];
+            int len = _matrix.GetLength(1);
+            try
             {
-                double[,] result = new double[matrix.GetLength(0),matrix.GetLength(1)];
-                for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int i = 0; i < matrix.Length; i++)
                 {
-                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    for (int j = 0; j < matrix[i].Length; j++)
                     {
-                        result[i, j] = matrix[i][j];
+                        if (matrix[i].Length != len) throw new ArgumentException();
+                        _matrix[i, j] = matrix[i][j];
                     }
                 }
-                _matrix = result;
             }
-            else
+            catch (Exception)
             {
-                throw new Exception("The array should be rectangular!");
+                Console.WriteLine("The array is not rectangular");
+                throw;
             }
-            //try
-            //{
-            //    for (int i = 0; i < matrix.GetLength(0); i++)
-            //    {
-            //        for (int j = 0; j < matrix.GetLength(1); j++)
-            //        {
-            //            _matrix[i, j] = matrix[i][j];
-            //        }
-            //    }
-            //}
-            //catch (Exception)
-            //{
-            //    throw new Exception("The array should be rectangular!");
-            //}
         }
         public MyMatrix(string[] matrix)
         {
@@ -69,29 +54,12 @@ namespace Lab2OOP
             }
             catch (Exception)
             {
-                throw new Exception("Amount of numbers in each string should be equal!");
+                throw new Exception("The array is not rectangular!");
             }
         }
-        public MyMatrix(string matrix)
-        {
-            try
-            {                
-                string[] stringIntoArray = matrix.Split("\\n");                
-                double[,] result = new double[stringIntoArray.Length, stringIntoArray[0].Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Length];
-                for (int i = 0; i < stringIntoArray.Length; i++)
-                {
-                    string[] input = stringIntoArray[i].Trim().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
-                    for (int j = 0; j < input.Length; j++)
-                    {                        
-                        result[i, j] = Convert.ToInt32(input[j]);
-                    }
-                }
-                _matrix = result;
-            }
-            catch (Exception)
-            {
-                throw new Exception("Can't create rectangular matrix. The string should follow input rules!");
-            }
+        public MyMatrix(string matrix) : this(matrix.Split("\n")) 
+        { 
+
         }
         //Properties
         public double Height
@@ -137,19 +105,7 @@ namespace Lab2OOP
         public void SetElement(int rowIndex, int colIndex, double value)
         {
             _matrix[rowIndex, colIndex] = value;
-        }
-        //Another case of making one of the constructors with the double[][] input
-        public bool IsRectangular(double[][] matrix)
-        {
-            foreach (double[] array in matrix)
-            {
-                if (array.Length != matrix[0].Length)
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
+        }        
         public override string ToString()
         {
             StringBuilder result = new StringBuilder();
--- Lab2OOP/MatrixOperations.cs
+++ Lab2OOP/MatrixOperations.cs
@@ -1,45 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Lab2OOP
 {
     partial class MyMatrix
     {        
         public static MyMatrix operator +(MyMatrix firstMatrix, MyMatrix secondMatrix)
-        {
-            int firstMatrixWidth = firstMatrix._matrix.GetLength(1);
-            int firstMatrixHeight = firstMatrix._matrix.GetLength(0);
-            int secondMatrixWidth = secondMatrix._matrix.GetLength(1);
-            int secondMatrixHeight = secondMatrix._matrix.GetLength(0);
-
-            if (firstMatrixWidth == secondMatrixWidth && firstMatrixHeight == secondMatrixHeight)
+        {            
+            try
             {
-                double[,] sumArray = new double[firstMatrixHeight, firstMatrixWidth];
-                for (int i = 0; i < firstMatrixHeight; i++)
+                double[,] sumArray = new double[firstMatrix._matrix.GetLength(0), firstMatrix._matrix.GetLength(1)];
+                for (int i = 0; i < firstMatrix._matrix.GetLength(0); i++)
                 {
-                    for (int j = 0; j < firstMatrixWidth; j++)
+                    for (int j = 0; j < firstMatrix._matrix.GetLength(1); j++)
                     {
                         sumArray[i, j] = firstMatrix._matrix[i, j] + secondMatrix._matrix[i, j];
                     }
                 }
                 return new MyMatrix(sumArray);
             }
-            else
+            catch (Exception)
             {
-                throw new Exception("Matrix should be the same size!");
+                throw new Exception("Matrix hsould be the same size!");
             }
         }  
         public static MyMatrix operator *(MyMatrix firstMatrix, MyMatrix secondMatrix)
-        {
-            if (firstMatrix._matrix.GetLength(1) == secondMatrix._matrix.GetLength(0))
+        {                    
+            try
             {
-                double[,] result = new double[firstMatrix._matrix.GetLength(0), secondMatrix._matrix.GetLength(1)];
+                double[,] multiplicationArray = new double[firstMatrix._matrix.GetLength(0), secondMatrix._matrix.GetLength(1)];
                 double sum = 0;
                 for (int i = 0; i < firstMatrix._matrix.GetLength(0); i++)
-                {                    
+                {
                     for (int j = 0; j < secondMatrix._matrix.GetLength(1); j++)
                     {
                         sum = 0;
@@ -47,12 +40,12 @@ namespace Lab2OOP
                         {
                             sum += firstMatrix._matrix[i, k] * secondMatrix._matrix[k, j];
                         }
-                        result[i, j] = sum;
+                        multiplicationArray[i, j] = sum;
                     }
                 }
-                return new MyMatrix(result);
+                return new MyMatrix(multiplicationArray);
             }
-            else
+            catch (Exception)
             {
                 throw new Exception("The number of cols in first matrix should be equal to the number of rows in second matrix!");
             }
